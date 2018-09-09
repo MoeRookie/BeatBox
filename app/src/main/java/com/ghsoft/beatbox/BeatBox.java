9b90280886272b9f48f03b521c2bc3dd892df8cf
MoeRookie <MoeRookie@163.com>
2018-09-09 13:57:21 +0800

    将获取到的声音资产文件名清单转化为sound列表

--- app/src/main/java/com/ghsoft/beatbox/BeatBox.java
+++ app/src/main/java/com/ghsoft/beatbox/BeatBox.java
@@ -5,6 +5,8 @@ import android.content.res.AssetManager;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 为了能在应用中定位、管理记录以及播放声音资产资源
@@ -14,6 +16,7 @@ public class BeatBox {
     private static final String TAG = "BeatBox"; // 记录日志
     private static final String SOUNDS_FOLDER = "sample_sounds"; // 存储声音资产文件目录名
     private final AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
@@ -24,7 +27,7 @@ public class BeatBox {
      * 获取所有声音资产的文件名
      */
     private void loadSounds() {
-        String[] soundNames;
+        String[] soundNames = null;
         try {
             soundNames = mAssets.list(SOUNDS_FOLDER);
             Log.i(TAG, "Found " + soundNames.length + " sounds");
@@ -32,5 +35,14 @@ public class BeatBox {
             ioe.printStackTrace();
             Log.e(TAG, "Could not list assets",ioe);
         }
+        for (String fileName : soundNames) { // 遍历声音资产文件名清单,转化为sound列表
+            String assetPath = SOUNDS_FOLDER + "/" + fileName;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
     }
 }
