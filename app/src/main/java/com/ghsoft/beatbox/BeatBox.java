bda85bd24fd28c02e06f4989069be3b9f2665a3d
MoeRookie <MoeRookie@163.com>
2018-09-10 09:39:39 +0800

    创建soundPool,以管理音频资产资源文件

--- app/src/main/java/com/ghsoft/beatbox/BeatBox.java
+++ app/src/main/java/com/ghsoft/beatbox/BeatBox.java
@@ -2,6 +2,8 @@ package com.ghsoft.beatbox;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -16,10 +18,14 @@ public class BeatBox {
     private static final String TAG = "BeatBox"; // 记录日志
     private static final String SOUNDS_FOLDER = "sample_sounds"; // 存储声音资产文件目录名
     private final AssetManager mAssets;
+    private final SoundPool mSoundPool;
     private List<Sound> mSounds = new ArrayList<>();
+    private static final int MAX_SOUNDS = 5;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        // 这个旧的构造函数不赞成使用，但为了兼容性我们需要它
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
     }
 
