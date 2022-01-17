94337dd0cf55276e5e4958aa8265ab4e9a600d95
MoeRookie <MoeRookie@163.com>
2022-01-17 16:37:38 +0800

    移除原有的holder和adapter实现

--- app/src/main/java/com/ghsoft/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/ghsoft/beatbox/BeatBoxFragment.java
@@ -42,12 +42,6 @@ public class BeatBoxFragment extends Fragment {
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
-        // todo=> 待删除
-        // View view = inflater.inflate(R.layout.fragment_beat_box, container, false);
-        // RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
-        // recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-        // recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
-        // return view;
     }
     private class SoundHolder extends RecyclerView.ViewHolder{
         private ListItemSoundBinding mBinding;
@@ -91,53 +85,6 @@ public class BeatBoxFragment extends Fragment {
             return mSounds.size();
         }
     }
-//    todo=>待删除
-//    private class SoundHolder extends RecyclerView.ViewHolder
-//    implements View.OnClickListener{
-//        private final Button mButton;
-//        private Sound mSound;
-//        public SoundHolder(LayoutInflater inflater, ViewGroup container) {
-//            super(inflater.inflate(R.layout.list_item_sound,container,false));
-//            mButton = itemView.findViewById(R.id.list_item_sound_button);
-//            mButton.setOnClickListener(SoundHolder.this);
-//        }
-//        public void bindSound(Sound sound){
-//            mSound = sound;
-//            mButton.setText(mSound.getName());
-//        }
-//
-//        @Override
-//        public void onClick(View v) {
-//            // 播放当前音频资产资源文件
-//            mBeatBox.play(mSound);
-//        }
-//    }
-//    todo=> 待删除
-//    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
-//        private List<Sound> mSounds;
-//
-//        public SoundAdapter(List<Sound> sounds) {
-//            mSounds = sounds;
-//        }
-//
-//        @NonNull
-//        @Override
-//        public SoundHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-//            LayoutInflater inflater = LayoutInflater.from(getActivity());
-//            return new SoundHolder(inflater,viewGroup);
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(@NonNull SoundHolder soundHolder, int i) {
-//            Sound sound = mSounds.get(i);
-//            soundHolder.bindSound(sound);
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return mSounds.size();
-//        }
-//    }
 
     @Override
     public void onDestroy() {
